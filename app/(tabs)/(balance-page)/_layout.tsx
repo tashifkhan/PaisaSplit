d62b5497375a04d75dfe36ad435c1f045c4c57c6
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-02-27 00:00:08 +0530

    added android animation stack

--- app/(tabs)/(balance-page)/_layout.tsx
+++ app/(tabs)/(balance-page)/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from 'expo-router/stack';
+import { Platform } from 'react-native';
 
 export default function Layout() {
   return (
     <Stack
       screenOptions={{
         headerShown: false,
+        animation: Platform.OS === 'android' ? 'fade_from_bottom' : 'default',
+        animationDuration: 200,
+        presentation: 'card',
+        contentStyle: {
+          backgroundColor: Platform.OS === 'android' ? '#fff' : 'transparent',
+        },
       }}
     />
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 
 declare global {
   interface Window {
